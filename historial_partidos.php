ae4de84eb4ada78b95b9a121e642d441b4d72de3
sgualdat <sgualdat@gmail.com>
2017-08-29 19:15:17 +0200

    Modificacion historial partidos

--- historial_partidos.php
+++ historial_partidos.php
@@ -56,6 +56,7 @@
         }
         else{
         $email=$row["email"];
+        $idEquipo=$row["idusuario"];
         }
     }
     ?>
@@ -121,18 +122,62 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body">
 
+                            <?php
+
+                             $link = mysqli_connect("127.0.0.1","root","","db696349657");
+                             $resultado=$link->query("SELECT * FROM `agenda` WHERE `estado` = 'PENDIENTE' OR `estado` = 'ACEPTADO'");
+
+                            ?>
+
                             <table class="table table-striped custab">
                                 <thead>
-
                                     <tr>
                                         <th>Hora</th>
                                         <th>Fecha</th>
                                         <th>Equipo</th>
                                         <th>Formato</th>
+                                        <th>ELO</th>
+                                        <th>Estado</th>
+                                        <th></th>
                                     </tr>
+                                    <?php
+                                        while($row=mysqli_fetch_array($resultado)){
+                                            $idPartido = $row["idagenda"];
+                                            if ($row["estado"] != 'PENDIENTE') {
+                                                $resultado1=$link->query("SELECT * FROM `equipo_partido` WHERE `idagenda` = '$idPartido' AND `idusuario` != '$idEquipo'");
+                                                $row1=mysqli_fetch_array($resultado1);
+                                                $idEquipoTemp = $row1["idusuario"];
+                                                $resultado2=$link->query("SELECT * FROM `usuario` WHERE `idusuario` = '$idEquipoTemp'");
+                                                $row2=mysqli_fetch_array($resultado2);
+                                                ?>
+                                                <tr>
+                                                <th><?php printf ("%s\n", $row["hora"]);?></th>
+                                                <th><?php printf ("%s\n", $row["fecha"]);?></th>
+                                                <th><?php printf ("%s\n", $row2["nombre_equipo"]);?></th>
+                                                <th><?php printf ("%s\n", $row["formato"]);?></th>
+                                                <th><?php printf ("%s\n", $row["elo"]);?></th>
+                                                <th><?php printf ("%s\n", $row["estado"]);?></th>
+                                                <th></th>
+                                                </tr>
+                                                <?php 
+                                            }
+                                            else{
+                                               ?>
+                                                <tr>
+                                                <th><?php printf ("%s\n", $row["hora"]);?></th>
+                                                <th><?php printf ("%s\n", $row["fecha"]);?></th>
+                                                <th><p>Sin equipo</p></th>
+                                                <th><?php printf ("%s\n", $row["formato"]);?></th>
+                                                <th><?php printf ("%s\n", $row["elo"]);?></th>
+                                                <th><?php printf ("%s\n", $row["estado"]);?></th>
+                                                <th><button id="cancelar" name="cancelar" class="btn btn-primary">Cancelar</button></th>
+                                                </tr>
+                                                <?php  
+                                            }    
+                                        }
+                                    ?>
                                 </thead>
                             </table>
-                            
                         </div>
                         <!-- /.panel-body -->
                     </div>
@@ -151,17 +196,45 @@
                         <!-- /.panel-heading -->
                         <div class="panel-body">
 
-                            <!-- Introducir buscador -->
+                            <?php
+
+                             $link = mysqli_connect("127.0.0.1","root","","db696349657");
+                             $resultado=$link->query("SELECT * FROM `agenda` WHERE `estado` = 'FINALIZADO'");
+
+                            ?>
 
                             <table class="table table-striped custab">
                                 <thead>
-
                                     <tr>
                                         <th>Hora</th>
                                         <th>Fecha</th>
                                         <th>Equipo</th>
                                         <th>Formato</th>
+                                        <th>ELO</th>
+                                        <th>Estado</th>
+                                        <th>Estadisticas</th>
                                     </tr>
+                                    <?php
+                                        while($row=mysqli_fetch_array($resultado)){
+                                                $idPartido = $row["idagenda"];
+                                                $resultado1=$link->query("SELECT * FROM `equipo_partido` WHERE `idagenda` = '$idPartido' AND `idusuario` != '$idEquipo'");
+                                                $row1=mysqli_fetch_array($resultado1);
+                                                $idEquipoTemp = $row1["idusuario"];
+                                                $resultado2=$link->query("SELECT * FROM `usuario` WHERE `idusuario` = '$idEquipoTemp'");
+                                                $row2=mysqli_fetch_array($resultado2);
+                                                ?>
+                                                <tr>
+                                                <th><?php printf ("%s\n", $row["hora"]);?></th>
+                                                <th><?php printf ("%s\n", $row["fecha"]);?></th>
+                                                <th><?php printf ("%s\n", $row2["nombre_equipo"]);?></th>
+                                                <th><?php printf ("%s\n", $row["formato"]);?></th>
+                                                <th><?php printf ("%s\n", $row["elo"]);?></th>
+                                                <th><?php printf ("%s\n", $row["estado"]);?></th>
+                                                <th><button id="consultarEstadisticas" name="consultarEstadisticas" class="btn btn-primary">Consultar</button></th>
+                                                </tr>
+                                                <?php     
+                                        }
+                                    ?>
                                 </thead>
                             </table>
                             
